74aa377ba0e1c325af24832402b40998cd76a3bb
Raphael Okocha-Ojeah <raphael.okocha-ojeah@pau.edu.ng>
2020-09-25 00:51:02 +0100

    post-validation and pre-fastify

---
+++ src/users/modules/roles/roles.service.ts
@@ -0,0 +1,22 @@
+
+export class RolesService {}
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateRolesDto } from './dto/create-roles.dto';
+import { Roles } from './models/roles.entity';
+
+@Injectable()
+export class RolessService {
+
+    constructor(
+        @InjectRepository(Roles) private roleRepository: Repository<Roles>,
+    ){}
+
+    async create (createRolesDto: CreateRolesDto): Promise<Roles>{
+
+        const newRoles = this.roleRepository.create(createRolesDto);
+        return this.roleRepository.save(newRoles);
+        
+    }
+}
